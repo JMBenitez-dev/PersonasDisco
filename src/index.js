097ed17a3cea223b2f91b418f552825f6a0571da
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-29 16:39:19 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+La idea es que investiguen como hacer la función aleatoria que pide el ejercicio y mostrar por consola las respuestas a las 3 preguntas que figuran.
+Pista: El nombre "aleatorio" de la función que figura en el ejercicio es un nombre genérico, no lo googleen no existe. Deben usar una función random.
+Cualquier duda sobre como resolver en este mismo hilo por favor. Gracias!
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
