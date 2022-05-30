c557e18ca20bef6450080c9c2b79c770085c7204
JMBenitez-dev <105891765+JMBenitez-dev@users.noreply.github.com>
2022-05-29 23:03:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,18 @@ function getRndInteger(min, max : number): number {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-
-
 for (let indice = 0; indice < cantPersonas.length; indice++) {
   
   cantPersonas[indice] = getRndInteger(19,40);
   if (cantPersonas[indice] < 21) {
-    menores21++
+    menores21++;
     
   } else{
-    mayores21++
+    mayores21++;
   } 
 }
 
 console.log("Total de Personas ",cantPersonas.length);
 console.log("Total de personas menores a 21 ",menores21);
 console.log("Total de personas mayores a 21 ",mayores21);
+//esta bien tu solucion, recordá que comentar el codigo y la prolijidad es parte de las buenas practicas.
